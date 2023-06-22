24ca2430eb28387116d56c86f76d7781dcbc32a3
ryansilva021 <ryansmithjr000@gmail.com>
2023-06-22 19:27:44 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,61 @@
+document.getElementById("agendamentoForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+  
+    var nomeInput = document.getElementById("nome");
+    var emailInput = document.getElementById("email");
+    var telefoneInput = document.getElementById("telefone");
+    var dataInput = document.getElementById("data");
+    var horarioInput = document.getElementById("horario");
+  
+    var nome = nomeInput.value.trim();
+    var email = emailInput.value.trim();
+    var telefone = telefoneInput.value.trim();
+    var data = dataInput.value;
+    var horario = horarioInput.value;
+  
+    if (!validarCampos(nome, email, telefone, data, horario)) {
+      return;
+    }
+  
+    var agendamento = {
+      nome: nome,
+      email: email,
+      telefone: telefone,
+      data: data,
+      horario: horario
+    };
+  
+    enviarAgendamento(agendamento);
+    exibirMensagem("Agendamento concluído com sucesso!");
+    limparFormulario();
+  });
+  
+  function validarCampos(nome, email, telefone, data, horario) {
+    if (nome === "" || email === "" || telefone === "" || data === "" || horario === "") {
+      alert("Por favor, preencha todos os campos do formulário.");
+      return false;
+    }
+  
+    // Outras validações necessárias, se houver
+  
+    return true;
+  }
+  
+  function enviarAgendamento(agendamento) {
+    // Lógica para enviar o agendamento para algum destino específico
+  }
+  
+  function exibirMensagem(mensagem) {
+    var mensagemAgendamento = document.getElementById("mensagemAgendamento");
+    mensagemAgendamento.textContent = mensagem;
+    mensagemAgendamento.style.display = "block";
+  
+    setTimeout(function() {
+      mensagemAgendamento.style.display = "none";
+    }, 2000);
+  }
+  
+  function limparFormulario() {
+    document.getElementById("agendamentoForm").reset();
+  }
+  
